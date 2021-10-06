4ac6d0bf38561525701497edb673286799375a29
josehenriqueroveda <zeh.henrique92@gmail.com>
2021-10-06 20:47:59 -0300

    Data visualization

--- Crop-Script.R
+++ Crop-Script.R
@@ -3,7 +3,8 @@
 ##################################################################################
 #Pacotes utilizados
 pacotes <- c("plotly","tidyverse","knitr","kableExtra","fastDummies","rgl","car",
-             "reshape2","jtools","lmtest","caret","pROC","ROCR","nnet","magick")
+             "reshape2","jtools","lmtest","caret","pROC","ROCR","nnet","magick",
+             "GGally")
 
 if(sum(as.numeric(!pacotes %in% installed.packages())) != 0){
   instalador <- pacotes[!pacotes %in% installed.packages()]
@@ -23,8 +24,6 @@ crop_data <- read_csv("Crop_recommendation.csv")
 
 glimpse(crop_data)
 
-crop_data$culture <- as.factor(crop_data$culture)
-
 crop_data %>% 
   kable() %>%
   kable_styling(bootstrap_options = "striped",
@@ -32,3 +31,47 @@ crop_data %>%
                 font_size = 14)
 
 summary(crop_data)
+
+##############################################################################
+#                             DATA VISUALIZATION                             #
+##############################################################################
+# Features Histogram
+ggplot(gather(crop_data[1:7]), aes(value, fill=key)) + 
+  geom_histogram(bins = 10) + 
+  facet_wrap(~key, scales = 'free_x')
+
+crop_data <- crop_data %>%
+  mutate(culture = factor(culture))
+
+# By Culture
+crop_data %>% 
+  pivot_longer(N:rainfall, names_to = "Feature", values_to = "value") %>% 
+  ggplot(aes(x = value, fill = culture)) +
+  geom_histogram(alpha = 0.7) +
+  labs(x = "Value", y = "Count", fill = NULL) +
+  theme_bw() +
+  facet_wrap(~Feature, scales = "free")
+
+# Density plots
+crop_data %>% 
+  pivot_longer(N:rainfall, names_to = "Feature", values_to = "value") %>% 
+  ggplot(aes(x = value, fill = culture)) +
+  geom_density(alpha = 0.5) +
+  labs(fill = NULL) +
+  theme_bw() +
+  facet_wrap(~Feature, scales = "free")
+
+# Box Plots
+crop_data %>% 
+  pivot_longer(N:rainfall, names_to = "Feature", values_to = "value") %>% 
+  ggplot(aes(x = culture, y = value, fill = culture)) +
+  geom_boxplot() +
+  labs(x = "Population", y = "Count", fill = NULL) +
+  theme_bw() +
+  coord_flip() +
+  facet_wrap(~Feature, scales = "free_x", ncol = 4) +
+  theme(legend.position = "top")
+
+# Understand the correlation between the features
+ggpairs(crop_data, columns = 1:7 ,title = "Correlation between features") 
+
